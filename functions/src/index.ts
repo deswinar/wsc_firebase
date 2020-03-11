cf146b2a97dd52e1d59fe2cdbe9fec2b4f165f07
deswinar <deswinauliyaarabbani@gmail.com>
2020-03-11 16:07:58 +0700

    Fix cannot reach API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as firebasHelper from 'firebase-functions-helper/dist';
+import * as firebaseHelper from 'firebase-functions-helper/dist';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
@@ -10,18 +10,34 @@ const db = admin.firestore();
 const app = express();
 const main = express();
 
+main.use('/api/v1', app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({extended: false}));
-main.use('api/v1', app);
 
-const sensorsCollection = 'sensors';
+const devicesCollection = 'devices';
 export const webApi = functions.https.onRequest(main);
 
-app.patch('/sensor/:sensorId', async(req, res) => {
+// View all devices
+app.get('/devices', (req, res) => {
+    firebaseHelper.firestore
+        .backup(db, devicesCollection)
+        .then(data => res.status(200).send(data))
+        .catch(error => res.status(400).send('Cannot get devices: ${error}'));
+})
+
+// View a device
+app.get('/devices/:deviceId', (req, res) => {
+    firebaseHelper.firestore
+        .getDocument(db, devicesCollection, req.params.deviceId)
+        .then(doc => res.status(200).send(doc))
+        .catch(error => res.status(400).send('Cannot get device: $(error)'));
+})
+
+app.patch('/devices/:deviceId', async(req, res) => {
     try{
-        await firebasHelper.firestore.updateDocument(db, sensorsCollection, req.params.sensorId, req.body);
+        await firebaseHelper.firestore.updateDocument(db, devicesCollection, req.params.deviceId, req.body);
         res.status(200).send('Update Success');
     }catch(error){
         res.status(204).send('Patch Error');
     }
-})
+});
